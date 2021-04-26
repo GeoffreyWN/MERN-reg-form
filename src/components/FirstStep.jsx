9d6fbe4c44baeb812a6140987d572dc4d6eb43ee
geof <>
2021-04-26 15:24:40 +0300

    initial commit

---
+++ src/components/FirstStep.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Form, Button } from 'react-bootstrap'
+
+const FirstStep = () => {
+    const { register, handleSubmit, errors } = useForm()
+
+    
+    return (
+        <div>
+            firts step from
+        </div>
+    )
+}
+
+export default FirstStep
